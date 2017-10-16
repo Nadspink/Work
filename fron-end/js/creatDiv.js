c29643eb63cb7914add1658387cd60a6bff15043
Nadspink <artyr20101@gmail.com>
2017-10-16 22:07:19 +0300

    ew

---
+++ fron-end/js/creatDiv.js
@@ -0,0 +1,8 @@
+function cret(){
+		var a= document.getElementById('a').value;
+		var block=document.createElement('div');
+		block.classList.add('messag');
+		block.innerHTML = a;
+		one.appendChild(block);
+		
+}
